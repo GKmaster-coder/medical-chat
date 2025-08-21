b5e791773de6b9e368c51cba7df3766f04a9feeb
Gkmaster-coder <gouravkashyap2468@gmail.com>
2025-08-21 11:33:53 +0530

    fix chat box height

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <Header/>
     <div className=" p-10 h-[90vh] flex items-center justify-center bg-gray-50 "
         style={{ backgroundImage: "url('https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')", backgroundSize: 'cover' }}>
-        <div className='h-[60vh] w-2xl'>
+        <div className='h-[70vh] w-2xl'>
           <InteractiveChat2 />
         </div>
       
